9c9602c13537e53f6c000babe027bb22a9d811ac
IamMichaell <robot.228532@gmail.com>
2023-05-30 22:03:54 +0300

    Добавьте файлы проекта.

---
+++ CastomView_Password/ViewModel/MainViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CastomView_Password.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _password;
+        private bool _isPasswordHidden;
+        private string _togglePasswordButtonImage;
+
+        public string Password
+        {
+            get { return _password; }
+            set { SetProperty(ref _password, value); }
+        }
+
+        public bool IsPasswordHidden
+        {
+            get { return _isPasswordHidden; }
+            set { SetProperty(ref _isPasswordHidden, value); }
+        }
+
+        public string TogglePasswordButtonImage
+        {
+            get { return _togglePasswordButtonImage; }
+            set { SetProperty(ref _togglePasswordButtonImage, value); }
+        }
+
+        public ICommand TogglePasswordCommand { get; private set; }
+
+        public MainViewModel()
+        {
+            //Установка пароля в скрытое состояние по умолчанию
+            TogglePasswordCommand = new Command(TogglePasswordVisibility);
+            IsPasswordHidden = true;
+            TogglePasswordButtonImage = "hide_eye_gray.png";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T backingStore, T value, [CallerMemberName] string propertyName = "", Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        //Смена значений по нажатию на кнопку с картинкой
+        private void TogglePasswordVisibility()
+        {
+            //Смена вилимости пароля
+            IsPasswordHidden = !IsPasswordHidden;
+            //Смена картинки
+            TogglePasswordButtonImage = IsPasswordHidden ? "hide_eye_gray.png" : "open_eye_gray.png";
+        }
+    }
+}
